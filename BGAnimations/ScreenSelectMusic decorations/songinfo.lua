c1259d868e0614a798759b0fe930bcb10d8b02d6
ca25nada <ca25nada@naver.com>
2015-03-25 17:31:23 -0600

    barebone stuff for screen select music

--- BGAnimations/ScreenSelectMusic decorations/songinfo.lua
+++ BGAnimations/ScreenSelectMusic decorations/songinfo.lua
@@ -1,10 +1,54 @@
 local t = Def.ActorFrame{};
 
 t[#t+1] = LoadFont("Common Normal") .. {
-	InitCommand=cmd(xy,300,300;visible,true);
+	Name="songTitle";
+	InitCommand=cmd(xy,100,200;visible,true;halign,0;zoom,0.45);
 	BeginCommand=function(self)
 		self:settext("uwaaaaa")
 	end;
+	SetCommand=function(self)
+		self:settext(GAMESTATE:GetCurrentSong():GetDisplayFullTitle())
+	end;
+	CurrentSongChangedMessageCommand=cmd(queuecommand,"Set");
 };
+
+t[#t+1] = LoadFont("Common Normal") .. {
+	Name="songArtist";
+	InitCommand=cmd(xy,100,210;visible,true;halign,0;zoom,0.45);
+	BeginCommand=function(self)
+		self:settext("uwaaaaa")
+	end;
+	SetCommand=function(self)
+		self:settext(GAMESTATE:GetCurrentSong():GetDisplayArtist())
+	end;
+	CurrentSongChangedMessageCommand=cmd(queuecommand,"Set");
+};
+
+t[#t+1] = LoadFont("Common Normal") .. {
+	Name="songGroup";
+	InitCommand=cmd(xy,100,190;visible,true;halign,0;zoom,0.45);
+	BeginCommand=function(self)
+		self:settext("uwaaaaa")
+	end;
+	SetCommand=function(self)
+		self:settext(GAMESTATE:GetCurrentSong():GetGroupName())
+	end;
+	CurrentSongChangedMessageCommand=cmd(queuecommand,"Set");
+};
+
+
+t[#t+1] = LoadFont("Common Normal") .. {
+	Name="songGroup";
+	InitCommand=cmd(xy,100,220;visible,true;halign,0;zoom,0.45);
+	BeginCommand=function(self)
+		self:settext("uwaaaaa")
+	end;
+	SetCommand=function(self)
+		self:settext(tostring(GAMESTATE:GetCurrentSteps(PLAYER_1):GetDifficulty())..tostring(GAMESTATE:GetCurrentSteps(PLAYER_1):GetMeter()))
+	end;
+	CurrentSongChangedMessageCommand=cmd(queuecommand,"Set");
+	CurrentStepsP1ChangedMessageCommand=cmd(queuecommand,"Set");
+};
+
 
 return t
