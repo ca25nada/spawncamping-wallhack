045248c26fcaf1d32d04703b060bafde56733cd0
ca25nada <ca25nada@naver.com>
2015-03-26 02:34:39 -0600

    whatamievendoing

--- BGAnimations/ScreenSelectMusic decorations/songinfo.lua
+++ BGAnimations/ScreenSelectMusic decorations/songinfo.lua
@@ -2,9 +2,10 @@ local t = Def.ActorFrame{};
 
 t[#t+1] = LoadFont("Common Normal") .. {
 	Name="songTitle";
-	InitCommand=cmd(xy,100,200;visible,true;halign,0;zoom,0.45);
+	InitCommand=cmd(xy,10,200;visible,true;halign,0;zoom,0.45);
 	BeginCommand=function(self)
 		self:settext("uwaaaaa")
+		self:diffuse(getMainColor(1))
 	end;
 	SetCommand=function(self)
 		local song = GAMESTATE:GetCurrentSong()
@@ -19,9 +20,10 @@ t[#t+1] = LoadFont("Common Normal") .. {
 
 t[#t+1] = LoadFont("Common Normal") .. {
 	Name="songArtist";
-	InitCommand=cmd(xy,100,210;visible,true;halign,0;zoom,0.45);
+	InitCommand=cmd(xy,10,210;visible,true;halign,0;zoom,0.45);
 	BeginCommand=function(self)
 		self:settext("uwaaaaa")
+		self:diffuse(getMainColor(1))
 	end;
 	SetCommand=function(self)
 		local song = GAMESTATE:GetCurrentSong()
@@ -36,9 +38,10 @@ t[#t+1] = LoadFont("Common Normal") .. {
 
 t[#t+1] = LoadFont("Common Normal") .. {
 	Name="songGroup";
-	InitCommand=cmd(xy,100,190;visible,true;halign,0;zoom,0.45);
+	InitCommand=cmd(xy,10,190;visible,true;halign,0;zoom,0.45);
 	BeginCommand=function(self)
 		self:settext("uwaaaaa")
+		self:diffuse(getMainColor(1))
 	end;
 	SetCommand=function(self)
 		local song = GAMESTATE:GetCurrentSong()
@@ -54,12 +57,18 @@ t[#t+1] = LoadFont("Common Normal") .. {
 
 t[#t+1] = LoadFont("Common Normal") .. {
 	Name="songGroup";
-	InitCommand=cmd(xy,100,220;visible,true;halign,0;zoom,0.45);
+	InitCommand=cmd(xy,10,220;visible,true;halign,0;zoom,0.45);
 	BeginCommand=function(self)
 		self:settext("uwaaaaa")
+		self:diffuse(getMainColor(1))
 	end;
 	SetCommand=function(self)
-		self:settext(tostring(GAMESTATE:GetCurrentSteps(PLAYER_1):GetDifficulty())..tostring(GAMESTATE:GetCurrentSteps(PLAYER_1):GetMeter()))
+		local steps = tostring(GAMESTATE:GetCurrentSteps(PLAYER_1))
+		if steps ~= nil then
+			self:settext(tostring(GAMESTATE:GetCurrentSteps(PLAYER_1):GetDifficulty())..tostring(GAMESTATE:GetCurrentSteps(PLAYER_1):GetMeter()))
+		else
+			self:settext("")
+		end
 	end;
 	CurrentSongChangedMessageCommand=cmd(queuecommand,"Set");
 	CurrentStepsP1ChangedMessageCommand=cmd(queuecommand,"Set");
--- Scripts/02 Colors.lua
+++ Scripts/02 Colors.lua
@@ -2,9 +2,19 @@
 themeColors = {
 	placeholder = {
 		[1] = color("#FFFFFF")
+	},
+	main = {
+		[1] = color("#00AEEF"), --Primary light blue
+		[2] = color("009AEF"),-- Slightly darker blue
+		[3] = color("00C2EF") -- Slightly lighter blue
 	}
+
 }
 
 function getPlaceholderColor()
 	return themeColors.placeholder[1] or color("#FFFFFF")
+end
+
+function getMainColor(i)
+	return themeColors.main[i] or color("#FFFFFF")
 end
