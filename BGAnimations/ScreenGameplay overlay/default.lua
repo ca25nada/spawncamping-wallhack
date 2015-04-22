2e2bfa64df91f5ccf90dc6608d42955cc2befdcc
unknown <ca25nada@naver.com>
2015-04-22 00:37:41 -0600

    barebone pacemaker graph implementation

--- BGAnimations/ScreenGameplay overlay/default.lua
+++ BGAnimations/ScreenGameplay overlay/default.lua
@@ -1,6 +1,7 @@
 t = Def.ActorFrame{}
 t[#t+1] = LoadActor("scoretracking")
 t[#t+1] = LoadActor("judgecount")
+t[#t+1] = LoadActor("pacemaker")
 t[#t+1] = LoadActor("ghostscore")
 t[#t+1] = LoadActor("progressbar")
 t[#t+1] = LoadActor("errorbar")
---
+++ BGAnimations/ScreenGameplay overlay/pacemaker.lua
@@ -0,0 +1,114 @@
+local ghostTypeP1 = tonumber(GetUserPref("GhostScoreTypeP1")); -- 1 = off, 2 = DP, 3 = PS, 4 = MIGS
+local targetP1 = (tonumber(GetUserPref("GhostTargetP1") or "0")+1)/100; -- target score from 0% to 100%.
+
+local ghostTypeP2 = tonumber(GetUserPref("GhostScoreTypeP2")); -- 1 = off, 2 = DP, 3 = PS, 4 = MIGS
+local targetP2 = (tonumber(GetUserPref("GhostTargetP2") or "0")+1)/100; -- target score from 0% to 100%.
+
+enabled = true
+
+player = PLAYER_1
+
+frameWidth = 105
+frameHeight = SCREEN_HEIGHT
+
+frameX = SCREEN_WIDTH-frameWidth
+frameY = 0
+
+barY = 420-- Starting point/bottom of graph
+barCount = 2 -- Number of graphs
+barWidth = 0.6
+barTrim = 0 -- Final width of each graph = ((frameWidth/barCount)*barWidth) - barTrim
+barHeight = 350 -- Max Height of graphs
+
+bottomTextY = barHeight+frameX
+
+local markerPoints = { --DP/PS/MIGS in that order.
+	[1] = {["AAA"] = THEME:GetMetric("PlayerStageStats", "GradePercentTier02"), 
+			["AA"] = THEME:GetMetric("PlayerStageStats", "GradePercentTier03") , 
+			["A"] = THEME:GetMetric("PlayerStageStats", "GradePercentTier04") , 
+			["B"] = THEME:GetMetric("PlayerStageStats", "GradePercentTier05") , 
+			["C"]=THEME:GetMetric("PlayerStageStats", "GradePercentTier06") , 
+			[""]=0 },
+
+	[2] = {["100%"]=1,["90%"]=0.9,["80%"]=0.8,["70%"]=0.7,["60%"]=0.6,["50%"]=0.5,[""]=0},
+	[3] = {["100%"]=1,["90%"]=0.9,["80%"]=0.8,["70%"]=0.7,["60%"]=0.6,["50%"]=0.5,[""]=0},
+}
+
+function currentScoreGraph(index,scoreType,color)
+	local t = Def.ActorFrame{}
+	t[#t+1] = Def.Quad{
+		InitCommand=cmd(xy,frameX+((1+(2*(index-1)))*(frameWidth/(barCount*2))),frameY+barY;zoomto,(frameWidth/barCount)*barWidth,1;valign,1;diffuse,color;diffusealpha,0.7;);
+		SetCommand=function(self)
+			curScore = getCurScoreST(player,scoreType)
+			maxScore = getMaxScoreST(player,scoreType)
+			if maxScore <= 0 then
+				self:zoomy(1)
+			else
+				self:zoomy(math.max(1,barHeight*(curScore/maxScore)))
+			end;
+		end;
+		JudgmentMessageCommand=cmd(queuecommand,"Set");
+	};
+	return t
+end;
+
+function targetScoreGraph(index,scoreType,color)
+	local t = Def.ActorFrame{}
+	t[#t+1] = Def.Quad{
+		InitCommand=cmd(xy,frameX+((1+(2*(index-1)))*(frameWidth/(barCount*2))),frameY+barY;zoomto,(frameWidth/barCount)*barWidth,1;valign,1;diffuse,color;diffusealpha,0.7;);
+		SetCommand=function(self)
+			curScore = getCurMaxScoreST(player,scoreType)*targetP1
+			maxScore = getMaxScoreST(player,scoreType)
+			if maxScore <= 0 then
+				self:zoomy(1)
+			else
+				self:zoomy(math.max(1,barHeight*(curScore/maxScore)))
+			end;
+		end;
+		JudgmentMessageCommand=cmd(queuecommand,"Set");
+	};
+	t[#t+1] = Def.Quad{
+		InitCommand=cmd(xy,frameX+((1+(2*(index-1)))*(frameWidth/(barCount*2))),frameY+barY;zoomto,(frameWidth/barCount)*barWidth,1;valign,1;diffuse,color;diffusealpha,0.7;);
+		SetCommand=function(self)
+			curScore = getCurMaxScoreST(player,scoreType)*targetP1
+			maxScore = getMaxScoreST(player,scoreType)
+			if maxScore <= 0 then
+				self:zoomy(1)
+			else
+				self:zoomy(math.max(1,barHeight*(curScore/maxScore)))
+			end;
+		end;
+		JudgmentMessageCommand=cmd(queuecommand,"Set");
+	};
+	return t
+end;
+
+function markers(scoreType,showMessage)
+	local t = Def.ActorFrame{
+		InitCommand=cmd(diffusealpha,0.3);
+	}
+	for k,v in pairs(markerPoints[scoreType]) do
+		t[#t+1] = Def.Quad{
+			InitCommand=cmd(xy,frameX,frameY+barY-(barHeight*v);zoomto,frameWidth,2;halign,0);
+		};
+		t[#t+1] = LoadFont("Common Normal")..{
+			InitCommand=cmd(xy,frameX,frameY+barY-(barHeight*v)-2;settext,k;zoom,0.3;halign,0;valign,1;)
+		};
+	end;
+	return t
+
+end;
+
+local t = Def.ActorFrame{}
+
+if enabled then
+	t[#t+1] = Def.Quad{
+		InitCommand=cmd(xy,frameX,frameY;zoomto,frameWidth,frameHeight;halign,0;valign,0;diffuse,color("#333333");diffusealpha,0.7;)
+	};
+	t[#t+1] = currentScoreGraph(1,ghostTypeP1-1,getMainColor(1))
+	t[#t+1] = targetScoreGraph(2,ghostTypeP1-1,getMainColor(2))
+	t[#t+1] = markers(ghostTypeP1-1,true)
+end;
+
+
+return t
