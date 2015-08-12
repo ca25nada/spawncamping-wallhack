d0c4ddf309341d91fe678baec1522002e2e6669c
unknown <ca25nada@naver.com>
2015-08-12 01:17:35 -0600

    more user friendly input for colorconfig

--- BGAnimations/ScreenColorChange overlay.lua
+++ BGAnimations/ScreenColorChange overlay.lua
@@ -1,5 +1,5 @@
 
-local cursor = 1
+local cursor = 0
 local count = 0
 local themeColor = themeConfig:get_data().color.main
 local colorTable = {}
@@ -13,7 +13,7 @@ local function scroller(index)
 	local t = Def.ActorFrame{}
 
 	t[#t+1] = LoadFont("Common Normal") .. {
-		InitCommand=cmd(xy,SCREEN_CENTER_X+index*10,SCREEN_CENTER_Y;zoom,0.4;);
+		InitCommand=cmd(xy,SCREEN_CENTER_X-60+index*20,SCREEN_CENTER_Y;zoom,0.8;);
 		OnCommand=function(self)
 			self:settext(string.format("%01X",number or 0))
 			if index == cursor then
@@ -51,6 +51,13 @@ local function scroller(index)
 					self:diffuse(color("#666666"))
 				end
 			end
+			if params.Name == "ColorStart" then
+				if index == cursor then
+					self:diffuse(color("#FFFFFF"))
+				else
+					self:diffuse(color("#666666"))
+				end
+			end
 		end;
 	}
 
@@ -64,8 +71,12 @@ local t = Def.ActorFrame{
 			SCREENMAN:GetTopScreen():Cancel()
 		end
 		if params.Name == "ColorStart" then
-			SCREENMAN:GetTopScreen():Cancel()
-			themeConfig:get_data().color.main = "#"..table.concat(colorTable)
+			if cursor < 6 then
+				cursor = ((cursor)%(count))+1
+			else
+				SCREENMAN:GetTopScreen():Cancel()
+				themeConfig:get_data().color.main = "#"..table.concat(colorTable)
+			end
 		end
 		if params.Name == "ColorRight" then
 			cursor = ((cursor)%(count))+1
@@ -76,6 +87,45 @@ local t = Def.ActorFrame{
 	end;
 }
 
+t[#t+1] = LoadActor("_frame");
+t[#t+1] = LoadFont("Common Large")..{
+	InitCommand=cmd(xy,5,32;halign,0;valign,1;zoom,0.55;diffuse,getMainColor(1);settext,"Color Config:";);
+}
+
+
+t[#t+1] = Def.Quad{
+	InitCommand=cmd(xy,SCREEN_CENTER_X,SCREEN_CENTER_Y+40;zoomto,200,30;);
+	OnCommand=function(self)
+		self:diffuse(color(themeColor))
+	end;
+	CodeMessageCommand=function(self,params)
+		if params.Name == "ColorUp" then
+			self:queuecommand("SetColor")
+		end
+		if params.Name == "ColorDown" then
+			self:queuecommand("SetColor")
+		end
+	end;
+	SetColorCommand=function(self)
+		self:diffuse(color("#"..table.concat(colorTable)))
+	end;
+}
+
+t[#t+1] = LoadFont("Common Normal") .. {
+	InitCommand=cmd(xy,SCREEN_CENTER_X-60,SCREEN_CENTER_Y;zoom,0.8;);
+	OnCommand=function(self)
+		self:settext("#")
+		self:diffuse(color("#666666"))
+	end;
+}
+
+t[#t+1] = LoadFont("Common Normal") .. {
+	InitCommand=cmd(xy,SCREEN_CENTER_X,SCREEN_CENTER_Y+100;zoom,0.4;);
+	OnCommand=function(self)
+		self:settext("This will change the main color of the theme.\n\nPress <Left>/<Right> to move cursor, <Up>/<Down> to change value.\nPress <Start> to confirm and <Back> to exit.\nPlease reload metrics after changing colors as some colors will not update unless you do so.")
+		self:diffuse(color("#FFFFFF"))
+	end;
+}
 
 for i=1,6 do
 	t[#t+1] = scroller(i)
