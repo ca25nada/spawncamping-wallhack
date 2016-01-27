a7f5b8d78e618446852e60073b5d11aedb2a184a
unknown <ca25nada@naver.com>
2016-01-26 21:48:51 -0700

    Fix errors in smo screens
    forgot to update this screen after the color prefs update oop

--- BGAnimations/ScreenNetSelectBase overlay/default.lua
+++ BGAnimations/ScreenNetSelectBase overlay/default.lua
@@ -33,7 +33,7 @@ t[#t+1] = LoadActor("../_frame");
 t[#t+1] = LoadActor("../_avatar");
 t[#t+1] = LoadActor("currentsort");
 t[#t+1] = LoadFont("Common Large")..{
-	InitCommand=cmd(xy,5,32;halign,0;valign,1;zoom,0.55;diffuse,getMainColor(1);settext,"Select Music:";);
+	InitCommand=cmd(xy,5,32;halign,0;valign,1;zoom,0.55;diffuse,getMainColor('highlight');settext,"Select Music:";);
 }
 
 t[#t+1] = LoadActor("../_cursor");
