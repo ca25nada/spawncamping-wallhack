aec0880acaeafde609c12cbe6ef8db689966d0c1
Prim <ca25nada@gmail.com>
2016-03-18 05:27:02 -0600

    New background effect
    random floating rectangles instead of snowing quads

--- BGAnimations/_particles.lua
+++ BGAnimations/_particles.lua
@@ -2,10 +2,10 @@ local t = Def.ActorFrame{}
 
 local enabled = themeConfig:get_data().global.Particles
 
-local particleAmount = 100
-local particleSize = 3
+local particleAmount = 25
 local dx = {}
 local dy = {}
+local alpha = {}
 
 local function isInScreen(particle)
 	local half = particleSize/2
@@ -22,36 +22,44 @@ local function resetPosition(particle)
 	return
 end;
 
-local function makeParticle(index,x,y,size,direction)
+
+local posX = {SCREEN_WIDTH,0}
+local posY = {SCREEN_HEIGHT,0}
+local function makeParticle(index)
 	return Def.Quad{
 		Name="Particle"..index;
-		InitCommand=cmd(xy,x,y;zoomto,size,size;rotationz,direction);
 		BeginCommand=function (self)
-			self:y(0)
+			self:sleep(index*0.1)
+			local size = math.random(20,50)
+			self:zoomto(size,size/5)
+			self:y(SCREEN_HEIGHT)
 			self:x(math.random(0,SCREEN_WIDTH))
-			self:diffusealpha((math.random()/2)+0.2)
-			dx[index] = 0
-			dy[index] = math.random()*5+1
-			self:sleep(math.random()*10)
+			self:diffuse(HSV(math.random(0,360),0.4,1))
+			dx[index] = 0--math.random()*5+1
+			dy[index] = math.random()*10+1
+			alpha[index] = math.random()*4
+			self:diffusealpha(alpha[index])
 			self:queuecommand('Move')
 		end;
 		MoveCommand = function(self,params)
 			self:finishtweening()
-			self:addy(dy[index])
-			self:addx(dx[index])
-			if not isInScreen(self) then
+			self:addy(dy[index]*((SCREEN_CENTER_Y-self:GetY())/2000))
+			if self:GetDiffuseAlpha() <= 0 then
 				self:sleep(1)
 				self:queuecommand('ResetPosition')
 			end;
-			self:sleep(1/60)
+			self:diffusealpha(clamp(alpha[index],0,0.8))
+			self:sleep(1/200)
 			self:queuecommand('Move')
+			alpha[index] = alpha[index] - 0.005
 		end;
 		ResetPositionCommand = function(self)
-			self:diffusealpha((math.random()/2)+0.2)
-			self:x(math.random(0,SCREEN_WIDTH))
-			self:y(0)
-			dx[index] = (INPUTFILTER:GetMouseX()-SCREEN_CENTER_X)/200
-			dy[index] = math.random()*4+1
+			alpha[index] = math.random()*2+2
+			dy[index] = math.random()*10+1
+			self:diffuse(HSV(math.random(0,360),0.4,1))
+			self:diffusealpha(alpha[index])
+				self:x(math.random(0,SCREEN_WIDTH))
+				self:y(SCREEN_HEIGHT)
 			self:queuecommand('Move')
 		end;
 	};
@@ -59,9 +67,10 @@ end;
 
 if enabled then
 	for i=1,particleAmount do
-		t[#t+1] = makeParticle(i,math.random(0,SCREEN_WIDTH),math.random(0,SCREEN_HEIGHT),particleSize,math.random()*360);
+		t[#t+1] = makeParticle(i);
 		dx[#dx+1] = 0
 		dy[#dy+1] = 0
+		alpha[#alpha+1] = 0
 	end;
 end;
 
