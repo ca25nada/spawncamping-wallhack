6a69eae2500e981717dad98b90b442545abc90bb
Prim <ca25nada@gmail.com>
2016-05-06 03:37:08 -0600

    Errorbar cleanup

--- BGAnimations/ScreenGameplay overlay/errorbar.lua
+++ BGAnimations/ScreenGameplay overlay/errorbar.lua
@@ -11,8 +11,8 @@ local pn = GAMESTATE:GetEnabledPlayers()[1]
 --=======================================
 --ONLY EDIT THESE VALUES
 --=======================================
-local timingScale = PREFSMAN:GetPreference("TimingWindowScale")
-local maxOffsetRange = 0.18*timingScale --timing range to show in seconds. 0.18 for upto bads, 0.135 for goods, 0.09 for greats, etc.
+local timingScale = PREFSMAN:GetPreference("TimingWindowScale") -- Timing window scale (e.g. 1.0 for J4)
+local maxOffsetRange = 0.18*timingScale -- timing range to show in seconds. 0.18 for upto bads, 0.135 for goods, 0.09 for greats, etc.
 local barcount = playerConfig:get_data(pn).ErrorBarMaxCount -- Number of bars to initialize. Older bars will just move to the newest offset before they fade out if it's not high enough.
 local frameX = SCREEN_CENTER_X -- X Positon (Center of the bar)
 local frameY = SCREEN_BOTTOM-35 -- Y Positon (Center of the bar)
@@ -23,30 +23,30 @@ local tickDuration = playerConfig:get_data(pn).ErrorBarDuration -- Time duration
 --=======================================
 
 
-local currentbar = 1
-local protimingsum = 0 -- unused
+local currentbar = 1 -- Index to be updated
 local offset = 0 -- offset of the last note hit
 
 function proTimingTicks(pn,index)
 	return Def.Quad{
 		InitCommand=cmd(xy,frameX,frameY;zoomto,tickWidth,frameHeight;diffusealpha,0;);
 		JudgmentMessageCommand=function(self,params)
-			if params.Player == pn and not params.HoldNoteScore then
-				if currentbar == index and 
-					params.TapNoteScore ~= 'TapNoteScore_HitMine' and 
-					params.TapNoteScore ~= 'TapNoteScore_AvoidMine' and 
-					params.TapNoteScore ~= 'TapNoteScore_Miss' and 
-					math.abs(offset) <= maxOffsetRange then
+			if currentbar == index and  params.Player == pn then
 
-					self:stoptweening()
-					self:diffusealpha(1)
-					self:diffuse(offsetToJudgeColor(offset))
-					self:x(frameX+(((offset)/maxOffsetRange)*(frameWidth/2)))
-					self:linear(tickDuration)
-					self:diffusealpha(0)
+				if params.TapNoteScore and not params.HoldNoteScore and
+				Enum.Reverse(TapNoteScore)[params.TapNoteScore] >= 5 and
+				Enum.Reverse(TapNoteScore)[params.TapNoteScore] < 10 then
+
+					if math.abs(offset) <= maxOffsetRange then
+						self:stoptweening()
+						self:diffusealpha(1)
+						self:diffuse(offsetToJudgeColor(offset))
+						self:x(frameX+(((offset)/maxOffsetRange)*(frameWidth/2)))
+						self:linear(tickDuration)
+						self:diffusealpha(0)
+					end
 				end
 			end
-		end;
+		end
 	}
 end
 
@@ -55,47 +55,42 @@ if enabled then
 		JudgmentMessageCommand=function(self,params)
 			if params.Player == pn then
 				
-				if params.HoldNoteScore then
-					--dosomething
-				elseif params.TapNoteScore == 'TapNoteScore_HitMine' or params.TapNoteScore == 'TapNoteScore_AvoidMine' then
-					--dosomething
-				else
-					if params.TapNoteScore ~= 'TapNoteScore_Miss' then
-						currentbar = ((currentbar)%barcount)+1
-						if params.Early then
-							offset = params.TapNoteOffset
-							protimingsum = protimingsum + params.TapNoteOffset
-						else
-							offset = params.TapNoteOffset
-							protimingsum = protimingsum + params.TapNoteOffset
-						end;
-					end;
-				end;
-			end;
-		end;
-	};
+				if params.TapNoteScore and not params.HoldNoteScore and
+				Enum.Reverse(TapNoteScore)[params.TapNoteScore] >= 5 and
+				Enum.Reverse(TapNoteScore)[params.TapNoteScore] < 10 then
+					currentbar = ((currentbar)%barcount)+1
+					if params.Early then
+						offset = params.TapNoteOffset
+					else
+						offset = params.TapNoteOffset
+					end
+				end
+			end
+		end
+	}
 
 	t[#t+1] = Def.Quad{
 		InitCommand=cmd(xy,frameX,frameY;zoomto,frameWidth,frameHeight;diffuse,color("#666666");diffusealpha,0.7);
-	};
+	}
 
 	-- Initialize a bunch of bars
 	for i=1,barcount do
 		t[#t+1] = proTimingTicks(pn,i)
-	end;
+	end
 
 	t[#t+1] = Def.Quad{
 		InitCommand=cmd(xy,frameX,frameY;zoomto,2,frameHeight;diffuse,color("#FFFFFF");diffusealpha,0.5);
-	};
+	}
 
 	t[#t+1] = LoadFont("Common Normal") .. {
         InitCommand=cmd(xy,frameX+frameWidth/4,frameY;zoom,0.35;);
         BeginCommand=cmd(settext,"Late";diffusealpha,0;smooth,0.5;diffusealpha,0.5;sleep,1.5;smooth,0.5;diffusealpha,0;);
-    };
+    }
+
     t[#t+1] = LoadFont("Common Normal") .. {
         InitCommand=cmd(xy,frameX-frameWidth/4,frameY;zoom,0.35;);
         BeginCommand=cmd(settext,"Early";diffusealpha,0;smooth,0.5;diffusealpha,0.5;sleep,1.5;smooth,0.5;diffusealpha,0;);
-    };
+    }
 
 end;
 
