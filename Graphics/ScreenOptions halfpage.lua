feacb6e899b600318f9ba36c06aefa62497f0449
Prim <ca25nada@gmail.com>
2016-05-14 14:28:23 -0600

    Fix avatar positioning in ScreenNestyOptions

--- Graphics/ScreenOptions halfpage.lua
+++ Graphics/ScreenOptions halfpage.lua
@@ -1,7 +1,7 @@
 local t = Def.ActorFrame{}
 
 t[#t+1] = Def.Quad{
-	InitCommand = function(self) self:zoomto(272,390):diffuse(color("#000000")):diffusealpha(0.8) end
+	InitCommand = function(self) self:zoomto(270,390):diffuse(color("#000000")):diffusealpha(0.8) end
 }
 
 return t
