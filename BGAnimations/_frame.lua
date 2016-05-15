35106fda3d2ef177d5286ce225ab26065a67a737
Prim <ca25nada@gmail.com>
2016-05-15 13:55:21 -0600

    Show current time on the bottom right of the screen

--- BGAnimations/_frame.lua
+++ BGAnimations/_frame.lua
@@ -38,6 +38,34 @@ t[#t+1] = Def.Quad{
 	end;
 }
 
+t[#t+1] = LoadFont("Common Normal") .. {
+	Name = "currentTime";
+	InitCommand=cmd(xy,SCREEN_WIDTH-5,SCREEN_HEIGHT-bottomFrameHeight/2;zoom,0.45;halign,1);
+	OnCommand = function(self)
+		self:y(SCREEN_HEIGHT+bottomFrameHeight/2)
+		self:smooth(0.5)
+		self:y(SCREEN_HEIGHT-bottomFrameHeight/2)
+	end;
+	OffCommand = function(self)
+		self:smooth(0.5)
+		self:y(SCREEN_HEIGHT+bottomFrameHeight/2)
+	end;
+};
+
+local function Update(self)
+	local year = Year()
+	local month = MonthOfYear()+1
+	local day = DayOfMonth()
+	local hour = Hour()
+	local minute = Minute()
+	local second = Second()
+	self:GetChild("currentTime"):settextf("%04d-%02d-%02d %02d:%02d:%02d",year,month,day,hour,minute,second)
+end;
+
+t.InitCommand=cmd(SetUpdateFunction,Update)
+
+
+
 --[[
 if themeConfig:get_data().global.TipType == 2 or themeConfig:get_data().global.TipType == 3 then
 	t[#t+1] = LoadFont("Common Normal")..{
