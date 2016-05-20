6e34d773f8140018fdb5e45418ae48e61e14dfae
Prim <ca25nada@gmail.com>
2016-05-20 16:54:52 -0600

    Background for ScreenMapController and blank actors for ScreenPrompt

---
+++ BGAnimations/ScreenMapControllers underlay.lua
@@ -0,0 +1,20 @@
+local t = Def.ActorFrame{}
+t[#t+1] = LoadActor("_background")
+t[#t+1] = LoadActor("_particles");
+t[#t+1] = Def.Quad{
+	InitCommand = function(self)
+		self:FullScreen()
+		self:Center()
+		self:diffuse(color("#000000")):diffusealpha(0)
+	end;
+	OnCommand = function(self)
+		self:smooth(0.5)
+		self:diffusealpha(0.8)
+	end;
+	OffCommand = function(self)
+		self:smooth(0.5)
+		self:diffusealpha(0)
+	end;
+}
+
+return t
