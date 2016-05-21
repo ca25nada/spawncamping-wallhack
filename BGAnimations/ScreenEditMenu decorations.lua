ff4615b039ec3f42d915a7f1b7a74e469d056b84
Prim <ca25nada@gmail.com>
2016-05-21 15:10:57 -0600

    Blank actors for ScreenEdit and ScreenMinuMenu

---
+++ BGAnimations/ScreenEditMenu decorations.lua
@@ -0,0 +1,19 @@
+local t = Def.ActorFrame{}
+
+t[#t+1] = Def.Quad{
+	InitCommand = function(self)
+		self:zoomto(SCREEN_WIDTH-25,SCREEN_HEIGHT-60)
+		self:Center()
+		self:diffuse(getMainColor("frame")):diffusealpha(0)
+	end;
+	OnCommand = function(self)
+		self:smooth(0.5)
+		self:diffusealpha(0.8)
+	end;
+	OffCommand = function(self)
+		self:smooth(0.5)
+		self:diffusealpha(0)
+	end;
+}
+
+return t
