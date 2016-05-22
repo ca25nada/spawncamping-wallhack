4ba0430901bfec416870e55e1c559235bed09e48
Prim <ca25nada@gmail.com>
2016-05-22 14:22:40 -0600

    Update 00 ThemeInfo.lua

--- Scripts/00 ThemeInfo.lua
+++ Scripts/00 ThemeInfo.lua
@@ -3,7 +3,7 @@
 themeInfo = {
 	Name = "spawncamping-wallhack",
 	Version = "2.0.0", -- a.b.c, a for complete overhauls, b for major releases, c for minor additions/bugfix.
-	Date = "20160512",
+	Date = "20160516",
 };
 
 function getThemeName()
