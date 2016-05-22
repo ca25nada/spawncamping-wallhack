b025f201cb3f6e887a2295dace4215d7a5491b1c
Prim <ca25nada@gmail.com>
2016-05-22 16:50:01 -0600

    Add MusicWheelItem Sort

---
+++ Graphics/MusicWheelItem Sort NormalPart.lua
@@ -0,0 +1,20 @@
+local t =  Def.ActorFrame{
+}
+
+
+t[#t+1] = Def.Quad{
+	InitCommand= function(self) 
+		self:x(0)
+		self:zoomto(capWideScale(get43size(340),340),44)
+		self:halign(0)
+	end;
+	SetCommand = function(self)
+		self:diffuse(getMainColor("frame"))
+		self:diffusealpha(0.9)
+	end;
+	BeginCommand = function(self) self:queuecommand('Set') end;
+	OffCommand = function(self) self:visible(false) end;
+}
+
+
+return t
