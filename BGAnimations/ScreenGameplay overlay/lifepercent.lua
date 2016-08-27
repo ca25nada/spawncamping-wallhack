28e94b7d2da25b36158bff7956ee9dc2d6794ba3
Prim <ca25nada@gmail.com>
2016-08-27 05:56:26 -0600

    Lifebar refactor

--- BGAnimations/ScreenGameplay overlay/lifepercent.lua
+++ BGAnimations/ScreenGameplay overlay/lifepercent.lua
@@ -1,88 +1,66 @@
 --Displays the current life percentage for each player
 
-local t = Def.ActorFrame {
-		Def.ActorFrame {
-			Name="Player1Life";
-			Def.Quad{
-				Name="Player1LifeBG";
-				InitCommand=cmd(xy,0,3;halign,0;valign,0;zoomto,270,13;faderight,0.5;diffusealpha,0.5);
-			};
-			Def.RollingNumbers{
-				Name="Player1LifeVal";
-				Font= "Common Normal", 
-				InitCommand= function(self)
-					self:xy(35,9):zoom(0.45):halign(1)
-				    self:set_chars_wide(2):set_text_format("%.0f%%"):set_approach_seconds(0.1)
-				end;
-			};
-		};
-
-		Def.ActorFrame {
-			Name="Player2Life";
-			Def.Quad{
-				Name="Player2LifeBG";
-				InitCommand=cmd(xy,SCREEN_WIDTH,3;halign,1;valign,0;zoomto,270,13;fadeleft,0.5;diffusealpha,0.5);
-			};
-			Def.RollingNumbers{
-				Name="Player2LifeVal";
-				Font= "Common Normal", 
-				InitCommand= function(self)
-					self:xy(SCREEN_WIDTH-35,9):zoom(0.45):halign(0)
-				    self:set_chars_wide(2):set_text_format("%.0f%%"):set_approach_seconds(0.1)
-				end;
-			};
-		};
-};
- 
 local function PLife(pn)
 	return STATSMAN:GetCurStageStats():GetPlayerStageStats(pn):GetCurrentLife() or 0
 end;
- 
-local function Update(self)
- 
-t.InitCommand=cmd(SetUpdateFunction,Update);
- 
-        if GAMESTATE:IsHumanPlayer(PLAYER_1) then
-        	self:GetChild("Player1Life"):GetChild("Player1LifeBG"):faderight(0.5+((1-PLife(PLAYER_1))*0.5))
-        	self:GetChild("Player1Life"):GetChild("Player1LifeBG"):zoomx(70+(PLife(PLAYER_1)*200))
-			self:GetChild("Player1Life"):GetChild("Player1LifeVal"):target_number(PLife(PLAYER_1)*100);
-			if PLife(PLAYER_1)*100 < 30 and PLife(PLAYER_1)*100 ~= 0 then -- replace with lifemeter danger later
-				self:GetChild("Player1Life"):diffuseshift()
-				self:GetChild("Player1Life"):effectcolor1(1,1,1,1)
-				self:GetChild("Player1Life"):effectcolor2(1,0.9,0.9,0.5)
-				self:GetChild("Player1Life"):effectperiod(0.9*PLife(PLAYER_1)+0.15)
-			elseif PLife(PLAYER_1)*100 <= 0 then
-				self:GetChild("Player1Life"):stopeffect()
-				self:GetChild("Player1Life"):diffuse(color("0,0,0,1"))
-			else
-				self:GetChild("Player1Life"):stopeffect()
-				self:GetChild("Player1Life"):diffuse(color("1,1,1,1"))
-			end;
-        else
-                self:GetChild("Player1Life"):visible(false)
-        end;
- 
-        if GAMESTATE:IsHumanPlayer(PLAYER_2) then
-        	self:GetChild("Player2Life"):GetChild("Player2LifeBG"):fadeleft(0.5+((1-PLife(PLAYER_2))*0.5))
-        	self:GetChild("Player2Life"):GetChild("Player2LifeBG"):zoomx(70+(PLife(PLAYER_2)*200))
-            self:GetChild("Player2Life"):GetChild("Player2LifeVal"):target_number(PLife(PLAYER_2)*100);
-			if PLife(PLAYER_2)*100 <= 30 and PLife(PLAYER_2)*100 ~= 0 then
-				self:GetChild("Player2Life"):diffuseshift()
-				self:GetChild("Player2Life"):effectcolor1(1,1,1,1)
-				self:GetChild("Player2Life"):effectcolor2(1,0.9,0.9,0.5)
-				self:GetChild("Player2Life"):effectperiod(0.9*PLife(PLAYER_2)+0.15)
-			elseif PLife(PLAYER_2)*100 <= 0 then
-				self:GetChild("Player2Life"):stopeffect()
-				self:GetChild("Player2Life"):diffuse(color("0,0,0,1"))
-			else
-				self:GetChild("Player2Life"):stopeffect()
-				self:GetChild("Player2Life"):diffuse(color("1,1,1,1"))		
-			end;
-        else
-                self:GetChild("Player2Life"):visible(false)
-        end;
-end;
 
-t.InitCommand=cmd(SetUpdateFunction,Update);
- 
+local t = Def.ActorFrame {}
+
+local function lifeBar(pn)
+	local t = Def.ActorFrame {
+		InitCommand = function(self) self:queuecommand("Set") end;
+		JudgmentMessageCommand = function(self) self:queuecommand("Set") end;
+		SetCommand = function(self)
+			self:GetChild(pn.."LifeBG"):faderight(0.5+((1-PLife(pn))*0.5))
+	        self:GetChild(pn.."LifeBG"):zoomx(70+(PLife(PLAYER_1)*200))
+	        self:GetChild(pn.."LifeVal"):target_number(PLife(pn)*100)
+        	if PLife(pn)*100 < 30 and PLife(pn)*100 ~= 0 then -- replace with lifemeter danger later
+				self:diffuseshift()
+				self:effectcolor1(1,1,1,1)
+				self:effectcolor2(1,0.9,0.9,0.5)
+				self:effectperiod(0.9*PLife(pn)+0.15)
+			elseif PLife(pn)*100 <= 0 then
+				self:stopeffect()
+				self:diffuse(color("0,0,0,1"))
+			else
+				self:stopeffect()
+				self:diffuse(color("1,1,1,1"))
+			end;
+		end;
+		Def.Quad{
+			Name = pn.."LifeBG";
+			InitCommand = function(self)
+				self:zoomto(270,13)
+				self:diffusealpha(0.5)
+				if pn == PLAYER_1 then
+					self:xy(0,3):halign(0):valign(0)
+					self:faderight(0.5)
+				else
+					self:xy(SCREEN_WIDTH,3):halign(1):valign(0)
+					self:fadeleft(0.5)
+				end
+				self:queuecommand("Set")
+			end;
+		};
+		Def.RollingNumbers{
+			Name = pn.."LifeVal";
+			Font = "Common Normal", 
+			InitCommand = function(self)
+				if pn == PLAYER_1 then
+					self:xy(35,9):zoom(0.45):halign(1)
+				else
+					self:xy(SCREEN_WIDTH-35,9):zoom(0.45):halign(0)
+				end
+			    self:set_chars_wide(2):set_text_format("%.0f%%"):set_approach_seconds(0.1)
+			    self:queuecommand("Set")
+			end;
+		};
+	}
+	return t
+end
+
+for _,pn in pairs(GAMESTATE:GetEnabledPlayers()) do
+	t[#t+1] = lifeBar(pn)
+end
+
 return t;
