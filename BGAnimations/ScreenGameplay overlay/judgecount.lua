798b6cc19f293cea8affa78792972f35b935f750
Prim <ca25nada@gmail.com>
2016-08-30 12:54:55 -0600

    Judgecount refactor (again)

--- BGAnimations/ScreenGameplay overlay/judgecount.lua
+++ BGAnimations/ScreenGameplay overlay/judgecount.lua
@@ -10,6 +10,10 @@ local judges = { -- do not edit
 	"HoldNoteScore_Held",
 	"HoldNoteScore_LetGo",
 }
+local judges2 = {}
+for k,v in pairs(judges) do
+	judges2[v] = true
+end
 
 local bareBone = isBareBone()
 local cols = GAMESTATE:GetCurrentStyle():ColumnsPerPlayer(); -- For relocating graph/judgecount frame
@@ -57,10 +61,30 @@ local t = Def.ActorFrame{}
 
 -- The Judgment text itself (MA for marvelous, etc.)
 local function judgeCounter(pn)
+	local highlight = playerConfig:get_data(pn_to_profile_slot(pn)).JudgeType == 2 and not bareBone
+
 	local t = Def.ActorFrame{
 		InitCommand = function(self)
 			self:xy(position[pn].X, position[pn].Y)
-		end
+		end;
+		JudgmentMessageCommand = function(self, params)
+			if params.Player == pn then
+				self:GetChild(pn.."Grade"):queuecommand("Set")
+				if judges2[params.TapNoteScore] or judges2[params.HoldNoteScore] then
+					if params.HoldNoteScore then
+						if highlight then
+							self:GetChild(pn..params.HoldNoteScore.."Highlight"):playcommand("Set")
+						end
+						self:GetChild(pn..params.HoldNoteScore.."Count"):queuecommand("Set")
+					elseif params.TapNoteScore then
+						if highlight then
+							self:GetChild(pn..params.TapNoteScore.."Highlight"):playcommand("Set")
+						end
+						self:GetChild(pn..params.TapNoteScore.."Count"):queuecommand("Set")
+					end
+				end
+			end
+		end;
 	}
 
 	t[#t+1] = Def.Quad{ -- Judgecount Background
@@ -71,6 +95,7 @@ local function judgeCounter(pn)
 	}
 
 	t[#t+1] = LoadFont("Common Normal") .. { --grade
+		Name=pn.."Grade";
 		InitCommand = function(self)
 			self:xy(5,8+(#judges*spacing)):halign(0)
 			self:zoom(gradeFontSize)
@@ -82,27 +107,23 @@ local function judgeCounter(pn)
 				self:settext(getGradeStrings(grade))
 			end
 		end;
-		JudgmentMessageCommand=cmd(queuecommand,"Set")
 	}
-
-	
 
 	for k,v in pairs(judges) do
 
-		if playerConfig:get_data(pn_to_profile_slot(pn)).JudgeType == 2 and not bareBone then
+		if highlight then
 			t[#t+1] = Def.Quad{ --JudgeHighlight
+				Name=pn..v.."Highlight";
 				InitCommand = function(self)
 					self:xy(0,5+((k-1)*spacing)):zoomto(frameWidth,5):halign(0):valign(0)
 					self:diffuse(color(colorConfig:get_data().judgment[v])):diffusealpha(0)
 				end;
-				JudgmentMessageCommand=function(self,params)
-					if (params.TapNoteScore == v or params.HoldNoteScore == v) and params.Player == pn then
-						self:stoptweening()
-						self:linear(0.1)
-						self:diffusealpha(highlightOpacity)
-						self:linear(0.5)
-						self:diffusealpha(0)
-					end
+				SetCommand=function(self)
+					self:stoptweening()
+					self:linear(0.1)
+					self:diffusealpha(highlightOpacity)
+					self:linear(0.5)
+					self:diffusealpha(0)
 				end
 			}
 		end
@@ -120,17 +141,13 @@ local function judgeCounter(pn)
 		}
 
 		t[#t+1] = LoadFont("Common Normal") .. {
+			Name=pn..v.."Count";
 			InitCommand = function(self)
 				self:xy(frameWidth-5,7+((k-1)*spacing)):zoom(judgeFontSize):halign(1)
 				self:settext(0)
 			end;
 			SetCommand=function(self)
 				self:settext(getJudgeST(pn,v))
-			end;
-			JudgmentMessageCommand=function(self,params)
-				if params.Player == pn then
-					self:queuecommand("Set")
-				end;
 			end;
 		}
 
