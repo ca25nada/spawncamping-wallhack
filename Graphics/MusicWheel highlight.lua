87f745df7d935bf7cbddce32d539cc1f65316053
Prim <ca25nada@gmail.com>
2016-09-08 14:56:02 -0600

    Revert to highlight color for musicwheel highlights when no steps are chosen.

--- Graphics/MusicWheel highlight.lua
+++ Graphics/MusicWheel highlight.lua
@@ -2,14 +2,20 @@ return Def.ActorFrame{
 	
 	Def.Quad{
 		Name="Horizontal";
-		InitCommand=cmd(x,-4;zoomto,capWideScale(get43size(348),348),52;halign,0;);
-		SetCommand=function(self)
+		InitCommand = function(self)
+			self:x(-4):zoomto(capWideScale(get43size(348),348),52):halign(0)
 			self:diffuseramp();
 			self:effectperiod(1)
 			self:effectcolor1(color("#FFFFFF00"));
-			self:effectcolor2(Alpha(getDifficultyColor(GAMESTATE:GetHardestStepsDifficulty()),0.2));
+			self:effectcolor2(Alpha(getMainColor("highlight"),0.2));
 		end;
-		CurrentSongChangedMessageCommand = function(self) self:queuecommand('Set') end;
+		SetCommand=function(self)
+			if GAMESTATE:GetCurrentSteps(GAMESTATE:GetEnabledPlayers()[1]) then
+				self:effectcolor2(Alpha(getDifficultyColor(GAMESTATE:GetHardestStepsDifficulty()),0.2));
+			else
+				self:effectcolor2(Alpha(getMainColor("highlight"),0.2));
+			end
+		end;
 		CurrentStepsP1ChangedMessageCommand = function(self) self:queuecommand('Set') end;
 		CurrentStepsP2ChangedMessageCommand = function(self) self:queuecommand('Set') end;
 	};
