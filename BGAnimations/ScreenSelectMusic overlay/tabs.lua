bca9cfad65e0579577098c258b58718b3177f6aa
Prim <ca25nada@gmail.com>
2017-09-27 05:35:50 -0600

    Allow music wheel scrolling with mousewheel and middle click dragging.

--- BGAnimations/ScreenSelectMusic overlay/tabs.lua
+++ BGAnimations/ScreenSelectMusic overlay/tabs.lua
@@ -16,18 +16,49 @@ local function input(event)
 			changeMusicRate(-0.05)
 		end
 
+		if event.DeviceInput.button == "DeviceButton_mousewheel up" then
+			wheel:Move(-1)
+			wheel:Move(0)
+		end
+
+		if event.DeviceInput.button == "DeviceButton_mousewheel down" then
+			wheel:Move(1)
+			wheel:Move(0)
+		end
+
+		if event.DeviceInput.button == "DeviceButton_middle mouse button" then
+			lastY = INPUTFILTER:GetMouseY()
+		end
+
 	end
 
-return false
+	if event.type == "InputEventType_Repeat" then
+		if event.DeviceInput.button == "DeviceButton_middle mouse button" then
+			curY = INPUTFILTER:GetMouseY()
+			if curY-lastY > 0 then
+				wheel:Move(math.floor((curY-lastY)/50))
+			elseif curY-lastY < 0 then
+				wheel:Move(math.ceil((curY-lastY)/50))
+			end
+			wheel:Move(0)
+		end
+	end
+
+	return false
 
 end
 
+local lastY
+local curY
+
 local top
+local wheel
 local t = Def.ActorFrame{
 	BeginCommand=function(self) resetTabIndex() end;
 	PlayerJoinedMessageCommand=function(self) resetTabIndex() end;
 	OnCommand = function(self)
 		top = SCREENMAN:GetTopScreen()
+		wheel = SCREENMAN:GetTopScreen():GetMusicWheel()
 		top:AddInputCallback(input)
 		self:diffusealpha(0)
 		self:smooth(0.5)
