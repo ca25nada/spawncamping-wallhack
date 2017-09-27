258b6aa1617b64ffcb1e19f16e15f23f1d455dd0
Prim <ca25nada@gmail.com>
2017-09-26 19:56:29 -0600

    Update BPMDisplay upon changing rates.

--- Graphics/ScreenSelectMusic BPMDisplay.lua
+++ Graphics/ScreenSelectMusic BPMDisplay.lua
@@ -3,6 +3,9 @@ return Def.BPMDisplay {
 	Name="BPMDisplay";
 	InitCommand=cmd(horizalign,left;zoom,0.50;);
 	SetCommand=function(self) self:SetFromGameState() end;
-	CurrentSongChangedMessageCommand=cmd(playcommand,"Set");
-	CurrentCourseChangedMessageCommand=cmd(playcommand,"Set");
+	CurrentSongChangedMessageCommand = function(self) self:playcommand("Set") end;
+	CurrentCourseChangedMessageCommand = function(self) self:playcommand("Set") end;
+	CurrentRateChangedMessageCommand = function(self) self:playcommand("Set") end;
+	CurrentStepsP1ChangedMessageCommand = function(self) self:playcommand("Set") end;
+	CurrentStepsP2ChangedMessageCommand = function(self) self:playcommand("Set") end;
 };
