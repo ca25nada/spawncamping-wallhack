276c5aab29171ff5c30bc72c4bf236af24b25e88
poco0317 <poco0317@users.noreply.github.com>
2019-01-18 17:51:56 -0600

    Fix the sample music playback (all modes)

--- BGAnimations/ScreenSelectMusic overlay/bgm.lua
+++ BGAnimations/ScreenSelectMusic overlay/bgm.lua
@@ -1,13 +1,13 @@
 local curSong = nil
 local start = math.max(0,GHETTOGAMESTATE:getLastPlayedSecond())
-local delay = 1
+local delay = 0.02
 local startFromPreview = true
 local loop = themeConfig:get_data().global.SongPreview == 2
 local curPath = ""
 local sampleStart = 0
 local musicLength = 0
 
-local test = true
+local sampleEvent = false
 
 GHETTOGAMESTATE:setLastPlayedSecond(0)
 
@@ -26,29 +26,37 @@ GHETTOGAMESTATE:setLastPlayedSecond(0)
 local deltaSum = 0
 local function playMusic(self, delta)
 	deltaSum = deltaSum + delta
-	if deltaSum > delay then
-		deltaSum = 0
-		if curSong and curPath then
-			if startFromPreview then -- When starting from preview point
-				SOUND:PlayMusicPart(curPath,sampleStart,musicLength-sampleStart,2,2,loop,true,true)
+	if deltaSum > delay and sampleEvent then
+		local s = SCREENMAN:GetTopScreen()
+		if s:GetMusicWheel() ~= nil then
+			if s:GetMusicWheel():IsSettled() then
+				deltaSum = 0
+				if curSong and curPath then
+					if startFromPreview then -- When starting from preview point
+						SOUND:PlayMusicPart(curPath,sampleStart,musicLength-sampleStart,2,2,loop,true,true)
+						self:SetUpdateFunctionInterval(musicLength-sampleStart)
 
-				if themeConfig:get_data().global.SongPreview == 3 then 
-					startFromPreview = false
+						if themeConfig:get_data().global.SongPreview == 3 then 
+							startFromPreview = false
+						end
+
+					else -- When starting from start of from exit point.
+						SOUND:PlayMusicPart(curPath,start,musicLength-start,2,2,false,true,false)
+						self:SetUpdateFunctionInterval(musicLength-start)
+						start = 0
+
+						if themeConfig:get_data().global.SongPreview == 2 then
+							startFromPreview = true
+						end
+
+					end
 				end
-
-			else -- When starting from start of from exit point.
-				SOUND:PlayMusicPart(curPath,start,musicLength-start,2,2,false,true,false)
-				start = 0
-
-				if themeConfig:get_data().global.SongPreview == 2 then
-					startFromPreview = true
-				end
-
 			end
 		end
-	end	
+	else
+		self:SetUpdateFunctionInterval(0.025)
+	end
 end
-
 
 local t = Def.ActorFrame{
 	InitCommand = function(self)
@@ -57,6 +65,7 @@ local t = Def.ActorFrame{
 		end
 	end,
 	CurrentSongChangedMessageCommand = function(self)
+		sampleEvent = false
 		SOUND:StopMusic()
 		deltaSum = 0
 		curSong = GAMESTATE:GetCurrentSong()
@@ -69,8 +78,18 @@ local t = Def.ActorFrame{
 			sampleStart = curSong:GetSampleStart()
 			musicLength = curSong:MusicLengthSeconds()
 			startFromPreview = start == 0
+			if themeConfig:get_data().global.SongPreview ~= 1 then
+				self:SetUpdateFunctionInterval(0.002)
+			end
 		end
 	end,
+	PlayingSampleMusicMessageCommand = function(self)
+		sampleEvent = true
+		if themeConfig:get_data().global.SongPreview ~= 1 then
+			self:SetUpdateFunctionInterval(0.002)
+			SOUND:StopMusic()
+		end
+	end
 }
 
 return t
