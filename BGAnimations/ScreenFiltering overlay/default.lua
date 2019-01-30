0d67fd9c0f86343c7b77840f537909425ea119a1
poco0317 <poco0317@users.noreply.github.com>
2019-01-29 20:39:25 -0600

    Initial filtering ui

--- BGAnimations/ScreenFiltering overlay/default.lua
+++ BGAnimations/ScreenFiltering overlay/default.lua
@@ -1,6 +1,12 @@
-local curInput = GHETTOGAMESTATE:getMusicSearch()
 local inputting = false
-
+local activeField
+local filterFields = {}
+filterFields["Lower"] = {}
+filterFields["Upper"] = {}
+for i = 1, #ms.SkillSets + 1 do -- the +1 is for the length field
+	filterFields["Lower"][i] = "0"
+	filterFields["Upper"][i] = "0"
+end
 
 local function input(event)
 	
@@ -31,6 +37,69 @@ local t = Def.ActorFrame {
 		top:AddInputCallback(input)
 	end
 }
+
+local boxHeight = 20
+local numBoxWidth = leftSectionWidth / 5
+local boundHorizontalSpacing = 8
+local boundVerticalSpacing = 2
+
+-- make the boxes for lower and upper bounds
+-- im gonna make this complicated and build an actorframe of 2 boxes
+-- you cant stop me
+local function boundBoxes(i)
+	local r = Def.ActorFrame {
+		-- Skillset name (or Length)
+		LoadFont("Common Normal") .. {
+			InitCommand = function(self)
+				self:xy(20,leftUpperSectionHeight + verticalSpacing + 30 + 30 + (boxHeight + boundVerticalSpacing)*i)
+				self:zoom(0.4)
+				self:halign(0)
+				self:settext(i == #ms.SkillSets+1 and "Length" or ms.SkillSets[i])
+			end
+		},
+		-- Lower bound box
+		quadButton(3) .. {
+			Name = "LowerBound"..(i == (#ms.SkillSets+1) and "Length" or ms.SkillSets[i]),
+			InitCommand = function(self)
+				self:xy(20 + numBoxWidth,leftUpperSectionHeight + verticalSpacing + 30 + 30 + (boxHeight + boundVerticalSpacing)*i)
+				self:halign(0)
+				self:zoomto(numBoxWidth,boxHeight)
+				self:diffusealpha(0.2)
+			end,
+			TopPressedCommand = function(self)
+				self:diffusealpha(0.4)
+				MESSAGEMAN:Broadcast("FilterBoundSelect", {bound = "Lower", selected = i})
+			end,
+			FilterBoundSelectMessageCommand = function(self, params)
+				if params.selected ~= i  or params.bound ~= "Lower" then
+					self:diffusealpha(0.2)
+				end
+			end
+		},
+		-- Upper bound box
+		quadButton(3) .. {
+			Name = "UpperBound"..(i == (#ms.SkillSets+1) and "Length" or ms.SkillSets[i]),
+			InitCommand = function(self)
+				self:xy(20 + numBoxWidth*2 + boundHorizontalSpacing,leftUpperSectionHeight + verticalSpacing + 30 + 30 + (boxHeight + boundVerticalSpacing)*i)
+				self:halign(0)
+				self:zoomto(numBoxWidth,boxHeight)
+				self:diffusealpha(0.2)
+			end,
+			TopPressedCommand = function(self)
+				self:diffusealpha(0.4)
+				MESSAGEMAN:Broadcast("FilterBoundSelect", {bound = "Upper", selected = i})
+			end,
+			FilterBoundSelectMessageCommand = function(self, params)
+				if params.selected ~= i or params.bound ~= "Upper" then
+					self:diffusealpha(0.2)
+				end
+			end
+		}
+
+	}
+
+	return r
+end
 
 t[#t+1] = LoadActor("../_mouse")
 
@@ -97,7 +166,67 @@ t[#t+1] = Def.ActorFrame {
 			self:settext("Filter Files by Rating")
 		end
 	},
+	LoadFont("Common Normal") .. {
+		InitCommand = function(self)
+			self:xy(20 + numBoxWidth,35)
+			self:halign(0)
+			self:zoom(0.3)
+			self:settext("Lower Bound")
+		end
+	},
+	LoadFont("Common Normal") .. {
+		InitCommand = function(self)
+			self:xy(20 + numBoxWidth*2 + boundHorizontalSpacing,35)
+			self:halign(0)
+			self:zoom(0.3)
+			self:settext("Upper Bound")
+		end
+	},
+	quadButton(6) .. {
+		InitCommand = function(self)
+			self:xy(leftSectionWidth - numBoxWidth - boundHorizontalSpacing, 20)
+			self:halign(0)
+			self:diffusealpha(0.2)
+			self:zoomto(numBoxWidth, 20)
+		end,
+		TopPressedCommand = function(self)
+			self:diffusealpha(0.4)
+			self:smooth(0.3)
+			self:diffusealpha(0.2)
+		end
+	},
+	LoadFont("Common Normal") .. {
+		InitCommand = function(self)
+			self:xy(leftSectionWidth - (numBoxWidth + boundHorizontalSpacing)/2 - 5, 20)
+			self:zoom(0.4)
+			self:settext("Apply")
+		end
+	},
+	quadButton(6) .. {
+		InitCommand = function(self)
+			self:xy(leftSectionWidth - numBoxWidth - boundHorizontalSpacing, 50)
+			self:halign(0)
+			self:diffusealpha(0.2)
+			self:zoomto(numBoxWidth, 20)
+		end,
+		TopPressedCommand = function(self)
+			self:diffusealpha(0.4)
+			self:smooth(0.3)
+			self:diffusealpha(0.2)
+		end
+	},
+	LoadFont("Common Normal") .. {
+		InitCommand = function(self)
+			self:xy(leftSectionWidth - (numBoxWidth + boundHorizontalSpacing)/2 - 5, 50)
+			self:zoom(0.4)
+			self:settext("Reset")
+		end
+	}
 }
+
+for i = 1, #filterFields["Lower"] do
+	t[#t+1] = boundBoxes(i)	
+end
 
 -- The right container (The Tags Menu)
 t[#t+1] = Def.ActorFrame {
