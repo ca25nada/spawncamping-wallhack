63f6d061e487eef99026324bfd87d4bb1215775a
poco0317 <poco0317@users.noreply.github.com>
2019-02-01 20:55:16 -0600

    Implement song filtering by rating and length

--- BGAnimations/ScreenFiltering overlay/default.lua
+++ BGAnimations/ScreenFiltering overlay/default.lua
@@ -1,22 +1,98 @@
 local inputting = false
-local activeField
+local activeField = {}
 local filterFields = {}
 filterFields["Lower"] = {}
 filterFields["Upper"] = {}
+
+local boundSection
+local everything
+
+local function wheelSearch()
+	GHETTOGAMESTATE:getSSM():GetMusicWheel():SongSearch(GHETTOGAMESTATE:getMusicSearch())
+end
+
+local function updateFilter()
+	for i = 1, #ms.SkillSets + 1 do
+		FILTERMAN:SetSSFilter(tonumber(filterFields["Lower"][i]), i, 0)
+		FILTERMAN:SetSSFilter(tonumber(filterFields["Upper"][i]), i, 1)
+		--FILTERMAN:SetSSFilter(tonumber(filterFields[activeField[1]][activeField[2]]), tonumber(activeField[1]), activeField[2] == "Upper" and 1 or 0)
+	end
+	wheelSearch()
+end
+
+local function resetFilter()
+	FILTERMAN:ResetSSFilters()
+	for i = 1, #ms.SkillSets + 1 do -- the +1 is for the length field
+		filterFields["Lower"][i] = FILTERMAN:GetSSFilter(i, 0)
+		filterFields["Upper"][i] = FILTERMAN:GetSSFilter(i, 1)
+	end
+	wheelSearch()
+end
+
 for i = 1, #ms.SkillSets + 1 do -- the +1 is for the length field
-	filterFields["Lower"][i] = "0"
-	filterFields["Upper"][i] = "0"
+	filterFields["Lower"][i] = FILTERMAN:GetSSFilter(i, 0)
+	filterFields["Upper"][i] = FILTERMAN:GetSSFilter(i, 1)
+end
+
+local function updateSelected()
+	local bound = activeField[1]
+	local i = activeField[2]
+	everything:GetChild("BoundContainer"..i):GetChild(bound):queuecommand("Set")
+end
+
+local function updateEverything()
+	for i = 1, #ms.SkillSets + 1 do
+		everything:GetChild("BoundContainer"..i):GetChild("Lower"):queuecommand("Set")
+		everything:GetChild("BoundContainer"..i):GetChild("Upper"):queuecommand("Set")
+	end
+end
+
+local function disableInput()
+	inputting = false
+	--activeField = {}
+	MESSAGEMAN:Broadcast("FilterBoundSelect", {})
 end
 
 local function input(event)
 	
 	if event.type == "InputEventType_FirstPress" then
-		if event.button == "Back" or event.button == "Start" then
+		if not inputting and (event.button == "Back" or event.button == "Start") then
 			SCREENMAN:GetTopScreen():Cancel()
+		elseif inputting then
+			local slotValue = tostring(filterFields[activeField[1]][activeField[2]])
+			if event.DeviceInput.button == "DeviceButton_left mouse button"  or event.DeviceInput.button == "DeviceButton_right mouse button" then
+				if not boundSection:isOver() then
+					disableInput()
+				end
+			elseif event.button == "Start" then
+				disableInput()
+				updateFilter()
+			elseif event.button == "Back" then
+				disableInput()
+			elseif tonumber(event.char) ~= nil then
+				if tonumber(slotValue .. event.char) ~= 0 and (slotValue .. event.char):len() < 6 then
+					filterFields[activeField[1]][activeField[2]] = tostring(tonumber(slotValue .. event.char))
+					updateSelected()
+				end
+			elseif event.DeviceInput.button == "DeviceButton_backspace" then
+				local nextValue = slotValue:sub(1,-2)
+				if nextValue ~= nil and tonumber(nextValue) ~= slotValue then
+					if nextValue == "" then
+						filterFields[activeField[1]][activeField[2]] = "0"
+					else
+						filterFields[activeField[1]][activeField[2]] = nextValue
+					end
+					updateSelected()
+				end
+			elseif event.DeviceInput.button == "DeviceButton_delete" then
+				filterFields[activeField[1]][activeField[2]] = "0"
+				updateSelected()
+			end
 		end
 	end
-	return false
 
+
+	return false
 end
 
 local top
@@ -33,6 +109,7 @@ local horizontalSpacing = 10
 
 local t = Def.ActorFrame {
 	OnCommand = function(self)
+		everything = self
 		top = SCREENMAN:GetTopScreen()
 		top:AddInputCallback(input)
 	end
@@ -48,6 +125,7 @@ local boundVerticalSpacing = 2
 -- you cant stop me
 local function boundBoxes(i)
 	local r = Def.ActorFrame {
+		Name = "BoundContainer"..i,
 		-- Skillset name (or Length)
 		LoadFont("Common Normal") .. {
 			InitCommand = function(self)
@@ -68,12 +146,27 @@ local function boundBoxes(i)
 			end,
 			TopPressedCommand = function(self)
 				self:diffusealpha(0.4)
+				inputting = true
+				activeField = {"Lower", i}
 				MESSAGEMAN:Broadcast("FilterBoundSelect", {bound = "Lower", selected = i})
 			end,
 			FilterBoundSelectMessageCommand = function(self, params)
 				if params.selected ~= i  or params.bound ~= "Lower" then
 					self:diffusealpha(0.2)
 				end
+			end
+		},
+		LoadFont("Common Normal") ..{
+			Name = "Lower",
+			InitCommand = function(self)
+				self:xy(25 + numBoxWidth,leftUpperSectionHeight + verticalSpacing + 30 + 30 + (boxHeight + boundVerticalSpacing)*i)
+				self:zoom(0.5)
+				self:halign(0)
+				self:settext("")
+				self:queuecommand("Set")
+			end,
+			SetCommand = function(self)
+				self:settext(filterFields["Lower"][i])
 			end
 		},
 		-- Upper bound box
@@ -87,12 +180,27 @@ local function boundBoxes(i)
 			end,
 			TopPressedCommand = function(self)
 				self:diffusealpha(0.4)
+				inputting = true
+				activeField = {"Upper", i}
 				MESSAGEMAN:Broadcast("FilterBoundSelect", {bound = "Upper", selected = i})
 			end,
 			FilterBoundSelectMessageCommand = function(self, params)
 				if params.selected ~= i or params.bound ~= "Upper" then
 					self:diffusealpha(0.2)
 				end
+			end
+		},
+		LoadFont("Common Normal") ..{
+			Name = "Upper",
+			InitCommand = function(self)
+				self:xy(25 + numBoxWidth*2 + boundHorizontalSpacing,leftUpperSectionHeight + verticalSpacing + 30 + 30 + (boxHeight + boundVerticalSpacing)*i)
+				self:zoom(0.5)
+				self:halign(0)
+				self:settext("")
+				self:queuecommand("Set")
+			end,
+			SetCommand = function(self)
+				self:settext(filterFields["Upper"][i])
 			end
 		}
 
@@ -157,6 +265,15 @@ t[#t+1] = Def.ActorFrame {
 			self:diffusealpha(0.8)
 		end,
 	},
+	Def.Quad {
+		InitCommand = function(self)
+			boundSection = self
+			self:xy(10 + numBoxWidth,42)
+			self:zoomto(numBoxWidth * 2 + boundHorizontalSpacing, boxHeight * (#ms.SkillSets+1) + boundVerticalSpacing * #ms.SkillSets)
+			self:halign(0):valign(0)
+			self:diffusealpha(0)
+		end
+	},
 	LoadFont("Common Bold") .. {
 		InitCommand = function(self)
 			self:xy(5,10)
@@ -183,6 +300,7 @@ t[#t+1] = Def.ActorFrame {
 		end
 	},
 	quadButton(6) .. {
+		Name = "ApplyButton",
 		InitCommand = function(self)
 			self:xy(leftSectionWidth - numBoxWidth - boundHorizontalSpacing, 20)
 			self:halign(0)
@@ -190,9 +308,11 @@ t[#t+1] = Def.ActorFrame {
 			self:zoomto(numBoxWidth, 20)
 		end,
 		TopPressedCommand = function(self)
+			self:finishtweening()
 			self:diffusealpha(0.4)
 			self:smooth(0.3)
 			self:diffusealpha(0.2)
+			updateFilter()
 		end
 	},
 	LoadFont("Common Normal") .. {
@@ -203,6 +323,7 @@ t[#t+1] = Def.ActorFrame {
 		end
 	},
 	quadButton(6) .. {
+		Name = "ResetButton",
 		InitCommand = function(self)
 			self:xy(leftSectionWidth - numBoxWidth - boundHorizontalSpacing, 50)
 			self:halign(0)
@@ -210,9 +331,12 @@ t[#t+1] = Def.ActorFrame {
 			self:zoomto(numBoxWidth, 20)
 		end,
 		TopPressedCommand = function(self)
+			self:finishtweening()
 			self:diffusealpha(0.4)
 			self:smooth(0.3)
 			self:diffusealpha(0.2)
+			resetFilter()
+			updateEverything()
 		end
 	},
 	LoadFont("Common Normal") .. {
