f6a333fbf0a615d255f51c8cbaa06e49b4ee9065
poco0317 <poco0317@users.noreply.github.com>
2019-03-21 22:45:57 -0500

    Allow pressing submenu button shortcuts slightly faster

--- BGAnimations/Screen in.lua
+++ BGAnimations/Screen in.lua
@@ -2,10 +2,10 @@ local t = Def.ActorFrame{}
 
 t[#t+1] = Def.Quad{
 	InitCommand=function(self)
-		self:FullScreen():diffuse(getMainColor("background")):diffusealpha(1)
+		self:FullScreen():diffuse(getMainColor("background")):diffusealpha(0):smooth(0.1):diffusealpha(1)
 	end,
 	OnCommand=function(self)
-		self:smooth(0.2):diffusealpha(0)
+		self:smooth(0.1):diffusealpha(0)
 	end
 }
 
