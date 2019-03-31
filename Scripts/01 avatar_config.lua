1cceb75ef7b30f4a35cd251923d7e235658e9fed
poco0317 <poco0317@users.noreply.github.com>
2019-03-31 03:32:20 -0500

    Add avatar loading util

--- Scripts/01 avatar_config.lua
+++ Scripts/01 avatar_config.lua
@@ -4,5 +4,15 @@ local defaultConfig = {
 	},
 }
 
+
 avatarConfig = create_setting("avatarConfig", "avatarConfig.lua", defaultConfig, 0)
 avatarConfig:load()
+
+
+function findAvatar(ID)
+	if avatarConfig:get_data().avatar[ID] ~= nil then
+		return avatarConfig:get_data().avatar[ID]
+	else
+		return defaultConfig.avatar.default
+	end
+end
