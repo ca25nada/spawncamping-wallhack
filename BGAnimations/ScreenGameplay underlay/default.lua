3b5178ee4c29cb17d7ad851f8d3b034e4719f92e
poco0317 <poco0317@users.noreply.github.com>
2019-03-31 20:22:33 -0500

    Fix permamirror and mini size

--- BGAnimations/ScreenGameplay underlay/default.lua
+++ BGAnimations/ScreenGameplay underlay/default.lua
@@ -1,12 +1,20 @@
+local playeroptions = GAMESTATE:GetPlayerState(PLAYER_1):GetPlayerOptions("ModsLevel_Preferred")
+playeroptions:Mini(2 - playerConfig:get_data(pn_to_profile_slot(PLAYER_1)).ReceptorSize / 50)
+local profile = PROFILEMAN:GetProfile(PLAYER_1)
+local replaystate = GAMESTATE:GetPlayerState(PLAYER_1):GetPlayerController() == "PlayerController_Replay"
+if profile:IsCurrentChartPermamirror() and not replaystate then -- turn on mirror if song is flagged as perma mirror
+	playeroptions:Mirror(true)
+end
+
+setMovableKeymode(getCurrentKeyMode())
+
+
 local t = Def.ActorFrame{}
-
-
 t[#t+1] = Def.Quad{
 	InitCommand=function(self)
 		self:xy(0,0):halign(0):valign(0):zoomto(SCREEN_WIDTH,30):diffuse(color("#00000099")):fadebottom(0.8)
 	end
 }
-setMovableKeymode(getCurrentKeyMode())
 
 
 return t
