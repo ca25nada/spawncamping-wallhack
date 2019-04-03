9af1fd1e1c12f85ef38645c8f1b6b1ca0f16b262
poco0317 <poco0317@users.noreply.github.com>
2019-04-02 23:24:49 -0500

    Fix some multi branching from main menu

--- Scripts/02 BranchOverrides.lua
+++ Scripts/02 BranchOverrides.lua
@@ -1,3 +1,18 @@
+function SMOnlineScreen()
+	if not IsNetSMOnline() then
+		return "ScreenSelectMusic"
+	end
+	for pn in ivalues(GAMESTATE:GetHumanPlayers()) do
+		if not IsSMOnlineLoggedIn(pn) then
+			return "ScreenSMOnlineLogin"
+		end
+	end
+	if not IsSMOnlineLoggedIn(pn) then
+		return "ScreenSMOnlineLogin"
+	end
+	return "ScreenNetRoom"
+end
+
 Branch.PlayerOptions= function()
 	local pm = GAMESTATE:GetPlayMode()
 	local restricted = { PlayMode_Oni= true, PlayMode_Rave= true,
@@ -18,10 +33,22 @@ Branch.AfterSelectProfile = function()
 	return "ScreenSelectMusic"
 end
 
+Branch.AfterNetSelectProfile = function()
+	return SMOnlineScreen()
+end
+
 Branch.AfterProfileLoad = function()
 	return "ScreenSelectMusic"
 end
 
 Branch.AfterTitleMenu = function()
 	return Branch.StartGame()
+end
+
+Branch.MultiScreen = function()
+	if IsNetSMOnline() then
+		return "ScreenNetSelectProfile"
+	else
+		return "ScreenNetworkOptions"
+	end
 end
