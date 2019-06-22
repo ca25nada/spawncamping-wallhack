efe8f0c7b06139fd96770c8a55eff4e1cee0e727
Barinade <poco0317@users.noreply.github.com>
2019-06-22 00:21:01 -0500

    Add Toasty
    Imported straight from Til Death for Etterna 0.66.1 with no modification

---
+++ BGAnimations/ScreenGameplay toasty/default.lua
@@ -0,0 +1,28 @@
+-- look for a custom lua file and if there is one load it instead
+if FILEMAN:DoesFileExist(getAssetPath("toasty").."/default.lua") then
+	local t = Def.ActorFrame {}
+	t[#t+1] = LoadActor("../../../../" .. getAssetPath("toasty") .. "/default")
+	return t
+ end
+
+local t =
+	Def.ActorFrame {
+	Def.Sprite {
+		InitCommand = function(self)
+			self:xy(SCREEN_WIDTH + 100, SCREEN_CENTER_Y)
+			self:Load(getToastyAssetPath("image"))
+		end,
+		StartTransitioningCommand = function(self)
+			self:decelerate(0.25):x(SCREEN_WIDTH - 100):sleep(1.75):accelerate(0.5):x(SCREEN_WIDTH + 100):linear(0):diffusealpha(0)
+		end
+	},
+	Def.Sound {
+		InitCommand = function(self)
+			self:load(getToastyAssetPath("sound"))
+		end,
+		StartTransitioningCommand = function(self)
+			self:play()
+		end
+	}
+}
+return t
