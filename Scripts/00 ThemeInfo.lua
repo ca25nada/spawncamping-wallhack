3b58c4d3f9d4e029f24fbb6e91d58a6eeb7b1d20
Barinade <poco0317@users.noreply.github.com>
2019-06-23 04:30:53 -0500

    Increment version date

--- Scripts/00 ThemeInfo.lua
+++ Scripts/00 ThemeInfo.lua
@@ -3,7 +3,7 @@
 themeInfo = {
 	Name = "spawncamping-wallhack (etterna .66.1)",
 	Version = "2.2.1", -- a.b.c, a for complete overhauls, b for major releases, c for minor additions/bugfix.
-	Date = "20190622",
+	Date = "20190623",
 }
 
 function getThemeName()
