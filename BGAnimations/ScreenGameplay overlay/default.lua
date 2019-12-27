42a08780df079da6da088345317cc47c7b3767da
Barinade <poco0317@users.noreply.github.com>
2019-12-26 23:09:35 -0600

    Remove old gameplay bpmdisplay

--- BGAnimations/ScreenGameplay overlay/default.lua
+++ BGAnimations/ScreenGameplay overlay/default.lua
@@ -22,7 +22,6 @@ if themeConfig:get_data().global.ProgressBar ~= 0 then
 end
 t[#t+1] = LoadActor("leaderboard")
 t[#t+1] = LoadActor("avatar")
---t[#t+1] = LoadActor("BPMDisplay")
 t[#t+1] = LoadActor("title")
 
 if inCustomize then
