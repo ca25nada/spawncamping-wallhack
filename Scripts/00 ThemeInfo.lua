26b6061f2b8261913297b25af6a8e21af941185d
Barinade <poco0317@users.noreply.github.com>
2020-01-07 22:08:37 -0600

    Update date to expected release date of 2.2.2 in themeinfo

--- Scripts/00 ThemeInfo.lua
+++ Scripts/00 ThemeInfo.lua
@@ -3,7 +3,7 @@
 themeInfo = {
 	Name = "spawncamping-wallhack (etterna .67.1)",
 	Version = "2.2.2", -- a.b.c, a for complete overhauls, b for major releases, c for minor additions/bugfix.
-	Date = "20191226",
+	Date = "20200108",
 }
 
 function getThemeName()
