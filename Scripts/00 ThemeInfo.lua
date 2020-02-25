43db8ebb415509faf1c47a4f5d37326649c0c7fc
Barinade <poco0317@users.noreply.github.com>
2020-02-24 20:13:09 -0600

    Increment version

--- Scripts/00 ThemeInfo.lua
+++ Scripts/00 ThemeInfo.lua
@@ -1,9 +1,9 @@
 -- theme identification file
 
 themeInfo = {
-	Name = "spawncamping-wallhack (etterna .67.1)",
-	Version = "2.2.2", -- a.b.c, a for complete overhauls, b for major releases, c for minor additions/bugfix.
-	Date = "20200108",
+	Name = "spawncamping-wallhack (etterna .68.1)",
+	Version = "2.2.3", -- a.b.c, a for complete overhauls, b for major releases, c for minor additions/bugfix.
+	Date = "20200225",
 }
 
 function getThemeName()
