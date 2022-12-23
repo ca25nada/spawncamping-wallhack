abfc8e101103b03d6197fd001d4fb4daec7bc7cd
Barinade <poco0317@users.noreply.github.com>
2022-12-23 16:19:04 -0600

    fix compatibility for etterna 0.72.0

--- BGAnimations/ScreenGameplay underlay/default.lua
+++ BGAnimations/ScreenGameplay underlay/default.lua
@@ -1,14 +1,8 @@
 local playeroptions = GAMESTATE:GetPlayerState():GetPlayerOptions("ModsLevel_Preferred")
 playeroptions:Mini(2 - playerConfig:get_data(pn_to_profile_slot(PLAYER_1)).ReceptorSize / 50)
-local profile = PROFILEMAN:GetProfile(PLAYER_1)
-local replaystate = GAMESTATE:GetPlayerState():GetPlayerController() == "PlayerController_Replay"
-if profile:IsCurrentChartPermamirror() and not replaystate then -- turn on mirror if song is flagged as perma mirror
-	playeroptions:Mirror(true)
-end
 
 setMovableKeymode(getCurrentKeyMode())
 setCurExp(PLAYER_1)
-
 
 local t = Def.ActorFrame{}
 t[#t+1] = Def.Quad{
