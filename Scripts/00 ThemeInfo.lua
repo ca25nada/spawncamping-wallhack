4e9e19181fa6da826b74320c3c09e377520c2840
Barinade <poco0317@users.noreply.github.com>
2024-12-25 16:55:26 -0600

    update version to 2.2.8

--- Scripts/00 ThemeInfo.lua
+++ Scripts/00 ThemeInfo.lua
@@ -1,9 +1,9 @@
 -- theme identification file
 
 themeInfo = {
-	Name = "spawncamping-wallhack (etterna .72.0)",
-	Version = "2.2.7", -- a.b.c, a for complete overhauls, b for major releases, c for minor additions/bugfix.
-	Date = "20221225",
+	Name = "spawncamping-wallhack (etterna .74.0)",
+	Version = "2.2.8", -- a.b.c, a for complete overhauls, b for major releases, c for minor additions/bugfix.
+	Date = "20241225",
 }
 
 function getThemeName()
